90802cfb53c36a4c34198612e25d125f73e8ba7c
shawnee <shanpan513@gmail.com>
2021-02-22 14:43:09 -0700

    stubbed out screens

--- app/src/main/java/team16/doglog/MainActivity.java
+++ app/src/main/java/team16/doglog/MainActivity.java
@@ -2,7 +2,9 @@ package team16.doglog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //called when user selects profile from main menu
+    public void selectProfile(View view){
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+
+    }
+
+    //called when user selects shots from main menu
+    public void selectShots(View view){
+        Intent intent = new Intent(this, Shots.class);
+        startActivity(intent);
+
+    }
+
+    //called when user selects office visits from main menu
+    public void selectOfficeVisits(View view){
+        Intent intent = new Intent(this, OfficeVisits.class);
+        startActivity(intent);
+
+    }
+
+    //called when user selects food from main menu
+    public void selectFood(View view){
+        Intent intent = new Intent(this, Food.class);
+        startActivity(intent);
+
+    }
+
+    //called when user selects medication from main menu
+    public void selectMedication(View view){
+        Intent intent = new Intent(this, Medication.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/team16/doglog/OfficeVisits.java
@@ -0,0 +1,14 @@
+package team16.doglog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfficeVisits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_office_visits);
+    }
+}
