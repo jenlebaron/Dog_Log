47906df3923b831a2282b04a2f34f7a301b41cc8
shawnee <shanpan513@gmail.com>
2021-03-03 15:03:33 -0700

    reminders screen layout

--- app/src/main/java/team16/doglog/BootReceiver.java
+++ app/src/main/java/team16/doglog/BootReceiver.java
@@ -1,13 +1,21 @@
 package team16.doglog;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        AlarmManager am = (AlarmManager)context.getSystemService(context.ALARM_SERVICE);
+        Intent i = new Intent(context, AlarmReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
 
+        SharedPreferences sharedPrefs = context.getSharedPreferences("Reminders", Context.MODE_PRIVATE);
+        am.set( AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5*1000, pi);
     }
 }
