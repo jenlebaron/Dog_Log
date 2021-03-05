62731a623a8f0b5cbf171894d868cd6571cbf91c
shawnee <shanpan513@gmail.com>
2021-03-05 09:13:24 -0700

    reminders working with user input (assuming user is entering correct input)

--- app/src/main/java/team16/doglog/AlarmReceiver.java
+++ app/src/main/java/team16/doglog/AlarmReceiver.java
@@ -15,78 +15,49 @@ import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.util.Map;
+
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
-       Log.d("message", "entered onReceive");
-
-
+        //create notification channel for reminders
         NotificationChannel channel = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             channel = new NotificationChannel("Reminders",
                     "Reminders Channel", NotificationManager.IMPORTANCE_HIGH);
-            Log.d("message", "line 1");
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
-            Log.d("message", "created channel");
         }
 
-
+        //create notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Reminders")
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle("Test Notification")
                 .setContentText("success")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        Log.d("message", "created notification");
 
+        //run notification
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
         notificationManagerCompat.notify(0, builder.build());
-        Log.d("message", "executed notification");
-
-       /* //method 1
-        context.runOnUiThread {
-                Toast.makeText(context, "Notification made!", Toast.LENGTH_SHORT).show();
-        }
-
-        //method 2
-        Handler(Looper.getMainLooper()).post {
-            Toast.makeText(context, "Notification made!", Toast.LENGTH_SHORT).show();
-        }
-
-        //method 3
-        Handler mainHandler = new Handler(context.getMainLooper());
-
-        Runnable myRunnable = new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(context, "Notification made!", Toast.LENGTH_SHORT).show();
-            } // This is your code
-        };
-        mainHandler.post(myRunnable);
-
-        //method 4
-    context.getMa
-        context.runOnUiThread(new Runnable() {
-            public void run() {
-                Toast.makeText(context, "Notification made!", Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
-
     }
 
     public void setAlarm(Context context){
-
         AlarmManager am = (AlarmManager)context.getSystemService(context.ALARM_SERVICE);
         Intent i = new Intent(context, AlarmReceiver.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
 
        SharedPreferences sharedPrefs = context.getSharedPreferences("Reminders", Context.MODE_PRIVATE);
-       am.set( AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5*1000, pi);
+       Map<String, ?> dates = sharedPrefs.getAll();
+       Long dateInMillis;
+       String key;
 
-
+        for (Map.Entry<String,?> e : dates.entrySet()){
+            key = e.getKey();
+            dateInMillis = (Long) e.getValue();
+            am.set(AlarmManager.RTC_WAKEUP, dateInMillis, pi);
+        }
     }
 }
--- app/src/main/java/team16/doglog/Presenter.java
+++ app/src/main/java/team16/doglog/Presenter.java
@@ -1,6 +1,29 @@
 package team16.doglog;
 
-public class Presenter {
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Presenter {
+    //stores dates for reminders in shared preferences
+    public void storeReminderDates(Map<String, Long> dates, Context context){
+        SharedPreferences sharedPrefs = context.getSharedPreferences("Reminders", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        String key;
+
+        for (Map.Entry<String,Long> e : dates.entrySet()){
+            key = e.getKey();
+            editor.putLong(key, e.getValue());
+        }
+
+        editor.apply();
+    }
+
+    public void setAlarms(Context context){
+        AlarmReceiver nm = new AlarmReceiver();
+        nm.setAlarm(context);
+    }
 
 }
