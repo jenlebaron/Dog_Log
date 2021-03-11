36fcfc5d49060ccdbb0016ba107fc9e6dd70ab4c
jenlebaron <leb20008@byui.edu>
2021-03-11 13:03:10 -0800

    shots and medication activity working properly.  It is able to save and load back on the page.

--- app/src/main/java/team16/doglog/Presenter.java
+++ app/src/main/java/team16/doglog/Presenter.java
@@ -42,36 +42,6 @@ public class Presenter {
         Log.d("message", sharedPrefs.getAll().toString());
     }
 
-//    //stores note for shots and medication in shared preferences
-//    public void saveShotsMedication(Context context, String distemper_shot_month,
-//                                    String distemper_shot_day, String distemper_shot_year,
-//                                    String rabies_shot_month, String rabies_shot_day,
-//                                    String rabies_shot_year, String parvo_shot_month,
-//                                    String parvo_shot_day, String parvo_shot_year,
-//                                    String hepatitis_shot_month, String hepatitis_shot_day,
-//                                    String hepatitis_shot_year, String other_shot_name,
-//                                    String other_shot_month, String other_shot_day,
-//                                    String other_shot_year, String medication_info) {
-//
-//        Log.d("message", "presenter saved shots and medication");
-//        SharedPreferences sharedPrefs = context.getSharedPreferences("ShotsMedication.txt", Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPrefs.edit();
-//        editor.putString("DISTEMPER_SHOTS_MONTH", distemper_shot_month);,
-//        ("DISTEMPER_SHOTS_DAY", distemper_shot_day);,
-//        ("DISTEMPER_SHOTS_YEAR", distemper_shot_year),("RABIES_SHOTS_MONTH", rabies_shot_month);,
-//        ("RABIES_SHOTS_DAY", rabies_shot_day),("RABIES_SHOTS_YEAR", rabies_shot_year);,
-//        ("PARVO_SHOTS_MONTH", parvo_shot_month);,
-//        ("PARVO_SHOTS_DAY", parvo_shot_day),("PARVO_SHOTS_YEAR", parvo_shot_year);,
-//        ("HEPATITIS_SHOTS_MONTH", hepatitis_shot_month),
-//        ("HEPATITIS_SHOTS_DAY", hepatitis_shot_day),("HEPATITIS_SHOTS_YEAR", hepatitis_shot_year),
-//        ("OTHER_SHOTS_NAME", other_shot_name),
-//        ("OTHER_SHOTS_MONTH", other_shot_month),("OTHER_SHOTS_DAY", other_shot_day),
-//        ("OTHER_SHOTS_YEAR", other_shot_year),("MEDICATION", medication_info);
-//        editor.apply();
-//
-//        Log.d("message", sharedPrefs.getAll().toString());
-//    }
-//}
 
     //stores note for shots and medication in shared preferences
     public void saveShotsMedication(Map<String, Long> dates, Context context){
