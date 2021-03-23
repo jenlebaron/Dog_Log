fbe96460e1b5ee8df2962157bcf3b6428a01300a
jenlebaron <leb20008@byui.edu>
2021-03-22 19:48:17 -0700

    code is up and running.  can save information amd toast is working.  Need to fix the top of form for it to say dog log instead of maim activity.

--- app/src/main/java/team16/doglog/MiscInformation.java
+++ app/src/main/java/team16/doglog/MiscInformation.java
@@ -1,6 +1,12 @@
 package team16.doglog;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,10 +17,49 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MiscInformation extends AppCompatActivity {
+    private int[] ids = new int[]{R.id.MiscFood, R.id.MiscRoutine, R.id.MiscInfoList};
+
+    private String[] keys = new String[]{"MiscFood", "MiscRoutine", "MiscInfoList"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_misc_information);
+
+        this.loadMiscInformation();
+    }
+
+    //called when activity is created
+    private void loadMiscInformation() {
+
+        Log.d("message", "entered loadMiscInformation");
+        SharedPreferences sharedPref = getSharedPreferences("MiscInformation", Context.MODE_PRIVATE);
+        Log.d("message", sharedPref.getAll().toString());
+
+        //get saved dates from shared preferences
+        String value;
+        EditText editText;
+        for (int i = 0; i < keys.length; i++) {
+            value = sharedPref.getString(keys[i], "");
+            editText = findViewById(ids[i]);
+            editText.setText(value);
+        }
+    }
+
+    //called when user presses save button
+    public void saveMiscInformation(View view) {
+        SharedPreferences sharedPrefs = this.getSharedPreferences("MiscInformation", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+
+        for (int j = 0; j < ids.length; j++) {
+            EditText t = (EditText) findViewById(ids[j]);
+            String value = t.getText().toString();
+            editor.putString(keys[j], value);
+
+        }
+        Toast.makeText(this, "Misc information saved", Toast.LENGTH_SHORT).show();
+        Log.d(getClass().getName(), "Received Toast");
+        editor.apply();
+
     }
 }
