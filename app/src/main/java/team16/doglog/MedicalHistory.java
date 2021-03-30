7c9a0b7d8ff56eedc4a0e897880977afcff347c7
Daphne Avril <daphneavril@outlook.com>
2021-03-30 16:47:56 +0200

    share note

--- app/src/main/java/team16/doglog/MedicalHistory.java
+++ app/src/main/java/team16/doglog/MedicalHistory.java
@@ -1,7 +1,18 @@
 package team16.doglog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -17,12 +28,94 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class MedicalHistory extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
 
+import android.widget.DatePicker;
+import android.widget.EditText;
+import java.util.Calendar;
+import android.widget.Button;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class MedicalHistory extends AppCompatActivity {
+    public static final String HEADER_MSG = "com.example.notes.MESSAGE";
+    public static final String EXTRA_MESSAGE = "com.example.notes.MESSAGE";
+    EditText date;
+    DatePickerDialog datePickerDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_history);
+        loadHistory();
+// initiate the date picker and a button
+        date = (EditText) findViewById(R.id.date);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(MedicalHistory.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        Intent intent = getIntent();
+        final String message = intent.getStringExtra(MedicalHistory.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        final TextView textView = findViewById(R.id.textView2);
+        textView.setText(message);
+        final String FILE_NAME = message + ".txt";
+        try {
+            FileInputStream fis = openFileInput(FILE_NAME);
+            BufferedReader reader = new BufferedReader( new InputStreamReader( fis ) );
+            String line;
+            String whole = "";
+            while ( (line = reader.readLine()) != null ) {
+                if(whole == "") {
+                    whole = whole + line;
+                }else{
+                    whole = whole + "\n" + line;
+                }
+            }
+            reader.close();
+            TextView textView1 = (TextView) findViewById(R.id.content);
+            textView1.setText(whole);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     //called when user presses save note button
@@ -30,21 +123,22 @@ public class MedicalHistory extends AppCompatActivity {
         Presenter presenter = new Presenter();
 
         //get dates and notes from user input
-        EditText userInput = (EditText) findViewById(R.id.medical_date);
+        EditText userInput = (EditText) findViewById(R.id.date);
         String date = userInput.getText().toString();
-        userInput.setText("");
+        // userInput.setText("");
         userInput = (EditText) findViewById(R.id.medical_history_notes);
         String notes = userInput.getText().toString();
-        userInput.setText("");
+        // userInput.setText("");
 
         //presenter will store note in shared preferences
         presenter.saveNote(this, date, notes);
 
         Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show();
+        loadHistory();
     }
 
     //called when user presses view notes button
-    public void loadHistory (View view){
+    public void loadHistory (){
         SharedPreferences sharedPrefs = this.getSharedPreferences("medicalHistoryNotes", Context.MODE_PRIVATE);
         Map<String, ?> notes = sharedPrefs.getAll();
         List<String> noteList = new ArrayList<>();
@@ -60,15 +154,25 @@ public class MedicalHistory extends AppCompatActivity {
                         android.R.layout.simple_list_item_1,
                         noteList);
         lv.setAdapter(adapter);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = lv.getItemAtPosition(position).toString();
+                Intent intent = new Intent(getApplicationContext(), ViewNote.class);
+                intent.putExtra(EXTRA_MESSAGE, item);
+                startActivity(intent);
+            }
+        });
     }
 
-    //called when user presses button to delete notes
     public void deleteNotes(View view){
         SharedPreferences sharedPrefs = this.getSharedPreferences("medicalHistoryNotes", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPrefs.edit();
         editor.clear();
         editor.commit();
-        loadHistory(view);
+        loadHistory();
     }
 
 
---
+++ app/src/main/java/team16/doglog/ViewNote.java
@@ -0,0 +1,112 @@
+package team16.doglog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        Intent intent = getIntent();
+        final String message = intent.getStringExtra(MedicalHistory.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        final TextView textView = findViewById(R.id.Heading);
+        textView.setText(message);
+        final String FILE_NAME = message + ".txt";
+
+        try {
+            FileInputStream fis = openFileInput(FILE_NAME);
+            BufferedReader reader = new BufferedReader( new InputStreamReader( fis ) );
+            String line;
+            String whole = "";
+            while ( (line = reader.readLine()) != null ) {
+                if(whole == "") {
+                    whole = whole + line;
+                }else{
+                    whole = whole + "\n" + line;
+                }
+            }
+            reader.close();
+            TextView textView1 = (TextView) findViewById(R.id.content);
+            textView1.setText(whole);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Button delete = (Button) findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File dir = getFilesDir();
+                File file = new File(dir, FILE_NAME);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                file.delete();
+                startActivity(intent);
+            }
+        });
+
+
+        Button share = (Button) findViewById(R.id.btnTextShare);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getText = message;
+                if (!getText.equals("") && getText.length() != 0)
+                    shareText(getText);
+                else
+                    Toast.makeText(ViewNote.this,
+                            "Please enter something to share.", Toast.LENGTH_SHORT)
+                            .show();
+            }
+        });
+    }
+
+
+
+
+
+
+
+    private void shareText(String text) {
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");// Plain format text
+
+        // You can add subject also
+        /*
+         * sharingIntent.putExtra( android.content.Intent.EXTRA_SUBJECT,
+         * "Subject Here");
+         */
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+        startActivity(Intent.createChooser(sharingIntent, "Share Text Using"));
+    }
+
+
+
+
+
+
+
+
+
+
+}
